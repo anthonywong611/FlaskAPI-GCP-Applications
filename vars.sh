2aed92fc6b54a6c331a6d4f9fc90a0dbef696aea
anthonywong611 <anthonywong2718@gmail.com>
2022-06-01 17:01:03 -0400

    removed files

--- vars.sh
+++
@@ -1,8 +0,0 @@
-WORKING_DIR=$(pwd)
-export GOOGLE_APPLICATION_CREDENTIALS=$WORKING_DIR/key.json
-export INSTANCE_HOST='127.0.0.1'
-export DB_PORT='5432'
-export DB_USER='postgres'
-export DB_PASS='Huangjianen611?'
-export DB_NAME='test'
-
