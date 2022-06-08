7f1646ef75a5c28a7293b3816b2ee5c3fee2ed03
anthonywong611 <anthonywong2718@gmail.com>
2022-06-08 18:50:52 -0400

    modified

--- deployment.sh
+++ deployment.sh
@@ -4,9 +4,5 @@ kubectl apply -f deployment.yaml
 # Create the service
 kubectl apply -f service.yaml 
 
-kubectl get deployments
-
-kubectl get pods
-
 # Get the external IP address of the service
 kubectl get services
